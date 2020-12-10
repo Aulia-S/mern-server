4a0b4f63ade1345a8085d88923a8693ffaecd18d
Aulia Sabri <auliasabri43@gmail.com>
2020-12-10 08:35:45 +0700

    menambahkan sign up & video 15

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-// test push
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+const connectDB = require('./database/db');
+const authRoutes = require('./routes/auth');
+
+// middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+
+connectDB();
+
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) =>{
+  res.send('hello world')
+})
+
+app.listen(port, console.log(`Listening on port http://localhost:${port}/`))
