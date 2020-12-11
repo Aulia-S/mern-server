9dd097df87148dcc773c01139362cfd1734c3d09
Aulia Sabri <auliasabri43@gmail.com>
2020-12-11 15:09:46 +0700

    vid 19

--- models/User.js
+++ models/User.js
@@ -15,6 +15,11 @@ const UserSchema = new mongoose.Schema(
     password : {
       type: String,
       required: true
+    },
+    
+    role: {
+      type: Number,
+      default: 0
     }
   },
 
