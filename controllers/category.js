93dff165fa4f5295558097d47ac67940bc6497ca
Aulia Sabri <aulia@localhost.localdomain>
2020-12-24 12:53:55 +0700

    vid 41

--- controllers/category.js
+++ controllers/category.js
@@ -4,6 +4,13 @@ exports.create = async (req, res) => {
   const { category } = req.body;
 
   try{
+    const categoryExist = await Category.findOne({ category });
+    if(categoryExist){
+      return res.status(400).json({
+        errorMessage: `${ category } already exists`
+      })
+    }
+
     let newCategory = new Category();
     newCategory.category = category;
 
@@ -15,6 +22,22 @@ exports.create = async (req, res) => {
 
   }catch(err){
     console.log('category create error : ', err);
+    res.status(500).json({
+      errorMessage: 'Please try again later'
+    })
+  }
+}
+
+exports.readAll = async (req, res) => {
+
+  try{
+    const categories = await Category.find({});
+
+    res.status(200).json({
+      categories: categories
+    })
+  }catch(err){
+    console.log('category readAll error : ', err);
     res.status(500).json({
       errorMessage: 'Please try again later'
     })
